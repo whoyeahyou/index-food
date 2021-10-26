01ce14299b2e31d28f9c29bd5481f2901b6b155f
whoyeahyou <64033933+whoyeahyou@users.noreply.github.com>
2021-10-26 15:15:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 const swiper = new Swiper('.swiper', {
+
      loop: true,
 
-     // If we need pagination
      pagination: {
           el: '.swiper-pagination',
      },
 
-     // Navigation arrows
      navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
